8e1814b714e4481436aee3633e43fb8f384a4b7f
jschmiss <julian.schmiss@holidaycheck.com>
2018-11-12 15:46:32 +0100

    first finance test

---
+++ dag_finance.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import os
+
+
+def start_venv():
+    os.system('source ~/PycharmProjects/banking_venv/bin/activate')
+
+
+def git_pull():
+    os.system('git pull ~/PycharmProjects/banking_app')
+
+
+def get_banks_data():
+    os.system('python3 main_banking_app.py --source_type banks --mode_database append ' +\
+              '--start_date ' + str(datetime.date(datetime.now())) + 'T00:00:00 ' +\
+              '--end_date ' + str(datetime.date(datetime.now())) + 'T23:59:59')
+
+
+dag = DAG('finance_data', description='Get data from finance source and write to database',
+          schedule_interval='59 23 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
+
+
+start_venv_operator = PythonOperator(task_id='start venv', python_callable=start_venv(), dag=dag)
+
+git_pull_operator = PythonOperator(task_id='pull from git', python_callable=git_pull(), dag=dag)
+
+get_banks_data_operator = PythonOperator(task_id='get data from banks', python_callable=get_banks_data(), dag=dag)
+
+start_venv_operator >> git_pull_operator
+git_pull_operator >> get_banks_data_operator
