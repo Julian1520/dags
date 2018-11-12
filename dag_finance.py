3acf459875425903c38bcd4457e4da5b91eb0176
jschmiss <julian.schmiss@holidaycheck.com>
2018-11-12 17:01:15 +0100

    update

--- dag_finance.py
+++ dag_finance.py
@@ -1,21 +1,6 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-import os
-
-
-def start_venv():
-    os.system('. ~/PycharmProjects/banking_venv/bin/activate')
-
-
-def git_pull():
-    os.system('git pull ~/PycharmProjects/banking_app')
-
-
-def get_banks_data():
-    os.system('python3 ~/PycharmProjects/banking_app/main_banking_app.py --source_type banks --mode_database append ' +\
-              '--start_date ' + str(datetime.date(datetime.now())) + 'T00:00:00 ' +\
-              '--end_date ' + str(datetime.date(datetime.now())) + 'T23:59:59')
 
 
 dag = DAG('finance_data', description='Get data from finance source and write to database',
@@ -26,7 +11,7 @@ start_venv_operator = BashOperator(task_id='start_venv',
                                    bash_command='. ~/PycharmProjects/banking_venv/bin/activate', dag=dag)
 
 git_pull_operator = BashOperator(task_id='pull_from_git',
-                                 bash_command='git pull ~/PycharmProjects/banking_app', dag=dag)
+                                 bash_command='cd ~/PycharmProjects/banking_app; git pull', dag=dag)
 
 get_banks_data_operator = BashOperator(task_id='get_data_from_bank',
                                        bash_command='python3 ~/PycharmProjects/banking_app/main_banking_app.py\
