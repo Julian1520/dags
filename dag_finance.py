b740f6f3ab934fd03d3e5d52f5f6add3c86f925f
jschmiss <julian.schmiss@holidaycheck.com>
2018-11-12 16:54:14 +0100

    update

--- dag_finance.py
+++ dag_finance.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
 import os
 
 
@@ -22,12 +22,17 @@ dag = DAG('finance_data', description='Get data from finance source and write to
           schedule_interval='59 23 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
+start_venv_operator = BashOperator(task_id='start_venv',
+                                   bash_command='. ~/PycharmProjects/banking_venv/bin/activate', dag=dag)
 
-start_venv_operator = PythonOperator(task_id='start_venv', python_callable=start_venv, dag=dag)
+git_pull_operator = BashOperator(task_id='pull_from_git',
+                                 bash_command='git pull ~/PycharmProjects/banking_app', dag=dag)
 
-git_pull_operator = PythonOperator(task_id='pull_from_git', python_callable=git_pull, dag=dag)
-
-get_banks_data_operator = PythonOperator(task_id='get_data_from_banks', python_callable=get_banks_data, dag=dag)
+get_banks_data_operator = BashOperator(task_id='get_data_from_bank',
+                                       bash_command='python3 ~/PycharmProjects/banking_app/main_banking_app.py\
+                                       --source_type banks --mode_database append ' +\
+                                       '--start_date ' + str(datetime.date(datetime.now())) + 'T00:00:00 ' +\
+                                       '--end_date ' + str(datetime.date(datetime.now())) + 'T23:59:59', dag=dag)
 
 start_venv_operator >> git_pull_operator
 git_pull_operator >> get_banks_data_operator
