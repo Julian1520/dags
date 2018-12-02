b118bd2e21bfd77b7ea6383496381b71166c28ce
Julian1520 <cobretti1520@freenet.de>
2018-12-02 16:09:54 +0100

    update

--- dag_finance.py
+++ dag_finance.py
@@ -16,20 +16,20 @@ git_pull_operator = BashOperator(task_id='pull_from_git',
 get_banks_data_operator = BashOperator(task_id='get_data_from_bank_giro',
                                        bash_command='~/PycharmProjects/banking_venv/bin/python ~/PycharmProjects/banking_app/main_banking_app.py \
                                        --source_type banks_giro --mode_database replace ' +\
-                                       '--start_date ' + str(datetime.date(datetime.now())) + 'T00:00:00 ' +\
-                                       '--end_date ' + str(datetime.date(datetime.now())) + 'T23:59:59 ', dag=dag)
+                                       '--start_date ' + str(datetime.date(datetime.now()+ timedelta(days=-1))) + 'T00:00:00 ' +\
+                                       '--end_date ' + str(datetime.date(datetime.now())) + 'T00:00:00 ', dag=dag)
 
 get_dkb_cc_data_operator = BashOperator(task_id='get_data_from_bank_cc',
                                        bash_command='~/PycharmProjects/banking_venv/bin/python ~/PycharmProjects/banking_app/main_banking_app.py \
                                        --source_type dkb_cc --mode_database replace ' +\
-                                       '--start_date ' + str(datetime.date(datetime.now())) + 'T00:00:00 ' +\
-                                       '--end_date ' + str(datetime.date(datetime.now()+ timedelta(days=-1))) + 'T00:00:00 ', dag=dag)
+                                       '--start_date ' + str(datetime.date(datetime.now()+ timedelta(days=-1))) + 'T00:00:00 ' +\
+                                       '--end_date ' + str(datetime.date(datetime.now())) + 'T00:00:00 ', dag=dag)
 
 get_dkb_depot_data_operator = BashOperator(task_id='get_data_from_bank_depot',
                                        bash_command='~/PycharmProjects/banking_venv/bin/python ~/PycharmProjects/banking_app/main_banking_app.py \
                                        --source_type dkb_depot --mode_database replace ' +\
-                                       '--start_date ' + str(datetime.date(datetime.now())) + 'T00:00:00 ' +\
-                                       '--end_date ' + str(datetime.date(datetime.now()+ timedelta(days=-1))) + 'T00:00:00 ', dag=dag)
+                                       '--start_date ' + str(datetime.date(datetime.now()+ timedelta(days=-1))) + 'T00:00:00 ' +\
+                                       '--end_date ' + str(datetime.date(datetime.now())) + 'T00:00:00 ', dag=dag)
 
 #start_venv_operator >> git_pull_operator
 git_pull_operator >> get_banks_data_operator
